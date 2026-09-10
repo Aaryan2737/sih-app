c9bb3353d7dcf96ce7a8813cc1f8f1e48f9a0417
Aaryan <aaryansp2737@gmail.com>
2026-09-11 04:41:01 +0530

    Fix: Remove finalized compileOptions block that caused Gradle failure

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -24,22 +24,6 @@ tasks.register<Delete>("clean") {
 }
 
 subprojects {
-    plugins.withId("com.android.library") {
-        configure<com.android.build.gradle.LibraryExtension> {
-            compileOptions {
-                sourceCompatibility = JavaVersion.VERSION_11
-                targetCompatibility = JavaVersion.VERSION_11
-            }
-        }
-    }
-    plugins.withId("com.android.application") {
-        configure<com.android.build.gradle.AppExtension> {
-            compileOptions {
-                sourceCompatibility = JavaVersion.VERSION_11
-                targetCompatibility = JavaVersion.VERSION_11
-            }
-        }
-    }
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
         compilerOptions.jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
     }
