11b41b4df21a7c7122925d73b059ee1e778c44dd
Aaryan <aaryansp2737@gmail.com>
2026-09-11 04:16:42 +0530

    Force JVM Target 17 for Java and Kotlin across all subprojects to resolve tflite_flutter compilation error

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -22,3 +22,20 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+
+subprojects {
+    afterEvaluate {
+        val androidPlugin = extensions.findByName("android")
+        if (androidPlugin != null) {
+            val clazz = androidPlugin.javaClass
+            val compileOptions = clazz.getMethod("getCompileOptions").invoke(androidPlugin)
+            val compileOptionsClass = compileOptions.javaClass
+            compileOptionsClass.getMethod("setSourceCompatibility", JavaVersion::class.java).invoke(compileOptions, JavaVersion.VERSION_17)
+            compileOptionsClass.getMethod("setTargetCompatibility", JavaVersion::class.java).invoke(compileOptions, JavaVersion.VERSION_17)
+        }
+
+        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+            compilerOptions.jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
+    }
+}
