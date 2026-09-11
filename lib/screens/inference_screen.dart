7a1e26392088a389f42b0fc856cfa5f7b1669c5e
Aaryan <aaryansp2737@gmail.com>
2026-09-11 16:13:45 +0530

    fix: import dart:typed_data for Uint8List to fix build error

--- lib/screens/inference_screen.dart
+++ lib/screens/inference_screen.dart
@@ -3,6 +3,7 @@ import 'package:tflite_flutter/tflite_flutter.dart';
 import 'package:image/image.dart' as img;
 import 'dart:io';
 import 'dart:math';
+import 'dart:typed_data';
 import '../data/local_database.dart';
 
 class ImagePreprocessor {
